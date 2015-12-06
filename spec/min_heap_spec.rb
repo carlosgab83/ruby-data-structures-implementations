c9c014652af441ab1beaebcc236ca25de58f5b98
Carlos Ochoa <cgochoa@acid.cl>
2015-12-06 19:47:38 -0300

    add min-heap specs

---
+++ spec/min_heap_spec.rb
@@ -0,0 +1,63 @@
+RSpec.describe MinHeap do
+
+	context "Class" do
+
+		let(:heap) { MinHeap.new }
+
+		it "responds to heapify" do
+			expect(heap).to respond_to(:heapify)
+		end
+
+		it "responds to insert" do
+			expect(heap).to respond_to(:insert)
+		end
+
+		it "responds to extract_min" do
+			expect(heap).to respond_to(:extract_min)
+		end
+
+	end
+
+	context "acts as min-heap" do
+
+		let(:heap) { MinHeap.new([16,15,14,14,13,12,11,10,9,8,7,6,5,4,3,2,1]) }
+
+		it "creates a correct min-heap" do
+			expect(heap.to_a).to eq [1,2,4,3,9,7,5,8,14,14,10,15,12,13,6,16,11]
+		end
+
+		it "retreive min priority elem" do
+			expect(heap.extract_min).to eq 1
+			expect(heap.extract_min).to eq 2
+			expect(heap.extract_min).to eq 3
+			expect(heap.extract_min).to eq 4
+			expect(heap.extract_min).to eq 5
+			expect(heap.extract_min).to eq 6
+			expect(heap.extract_min).to eq 7
+			expect(heap.extract_min).to eq 8
+			expect(heap.extract_min).to eq 9
+			expect(heap.extract_min).to eq 10
+			expect(heap.extract_min).to eq 11
+			expect(heap.extract_min).to eq 12
+			expect(heap.extract_min).to eq 13
+			expect(heap.extract_min).to eq 14
+			expect(heap.extract_min).to eq 14
+			expect(heap.extract_min).to eq 15
+			expect(heap.extract_min).to eq 16
+			expect(heap.extract_min).to eq nil
+		end
+
+		it "counts correctly num of elements" do
+			heap.extract_min
+			expect(heap.size).to eq 16
+		end
+
+		it "dont breaks with an empty array" do
+			heap = MinHeap.new []
+			expect(heap.size).to eq 0
+			heap.extract_min
+			expect(heap.size).to eq 0
+		end
+	end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,5 @@ RSpec.configure do |config|
 end
 
 require_relative '../max_heap.rb'
+require_relative '../min_heap.rb'
 require 'pry'
