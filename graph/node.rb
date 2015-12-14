940e1ddc199928eb791a58f7428f0dd04655ffb4
Carlos Ochoa <cgochoa@acid.cl>
2015-12-13 23:24:37 -0300

    re-indent file

--- graph/node.rb
+++ graph/node.rb
@@ -1,16 +1,16 @@
-  class Node
-  	attr_accessor :data, :edges
+class Node
+  attr_accessor :data, :edges
 
-  	def initialize(args = {})
-  		@data  = args[:data]
-  		@edges = args[:edges] || []
-  	end
-
-    def <=>(other_node)
-      data <=> other_node.data
-    end
-
-    def to_s
-      "Node #{data}"
-    end
+  def initialize(args = {})
+    @data  = args[:data]
+    @edges = args[:edges] || []
   end
+
+  def <=>(other_node)
+    data <=> other_node.data
+  end
+
+  def to_s
+    "Node #{data}"
+  end
+end
